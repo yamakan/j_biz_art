f0a44488946f4a8871d41de95e2d96af4f7ad50e
Kan Yammamoto <kanbasketball@gmail.com>
2012-12-25 11:08:03 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JBizArt::Application.config.secret_token = 'f4fb5f90a05afdc0867cc9450579ef717071f095420aa6bef450d912724ca58b4a332a6b0fecbd01aca0edbd363afdae9a64cb3c69fa6fe1d07273b3d3ba8aa7'
